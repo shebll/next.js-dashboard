199fbf56d631912707f529f60ea3df92b20125c3
shebll <shebblloll@gmail.com>
2024-04-29 15:28:11 +0300

    init the routes

---
+++ app/(dashboard)/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function InvoicePage() {
+  return <div>invoice page</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import desktopHeroImage from '@/public/hero-desktop.png';
+import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
+
+import clsx from 'clsx';
+
+import AcmeLogo from '@/app/ui/acme-logo';
+
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
+    <main className="flex min-h-screen flex-col p-4">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className={styles.shape}></div>
+          <p
+            className={clsx(
+              `text-xl text-gray-800 md:text-3xl md:leading-normal `,
+              lusitana.className,
+            )}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -26,6 +40,25 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src={desktopHeroImage}
+            width={729}
+            height={402}
+            sizes="calc(60vw - 253px)"
+            priority
+            quality={100}
+            alt="Screenshots of the dashboard project showing desktop version"
+            className="hidden bg-cover md:block"
+          />
+          <Image
+            src={'/hero-mobile.png'}
+            width={529}
+            height={402}
+            priority
+            quality={100}
+            alt="Screenshots of the dashboard project showing mobile version"
+            className="block md:hidden"
+          />
         </div>
       </div>
     </main>
